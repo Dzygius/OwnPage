cd0846efa5b030e13d68e32aab32bbfe3c5fd2b9
Dzygius <armanida297@gmail.com>
2020-04-09 01:33:25 +0300

    Veikia database login and register

--- database.js
+++ database.js
@@ -1,8 +1,8 @@
 const mysql = require('mysql2');
 const dbConnection = mysql.createPool({
-    host     : 'localhost', // MYSQL HOST NAME
-    user     : 'root',        // MYSQL USERNAME
-    password : 'Zorgas123',    // MYSQL PASSWORD
-    database : 'users'      // MYSQL DB NAME
+    host: 'localhost', // MYSQL HOST NAME
+    user: 'root', // MYSQL USERNAME
+    password: 'Zorgas123', // MYSQL PASSWORD
+    database: 'users' // MYSQL DB NAME
 }).promise();
 module.exports = dbConnection;
--- index.js
+++ index.js
@@ -1,37 +1,168 @@
-// Get the modal
-var modalIn = document.getElementById("In");
-var modalUp = document.getElementById("Up");
+const express = require('express');
+const path = require('path');
+const cookieSession = require('cookie-session');
+const bcrypt = require('bcrypt');
+const dbConnection = require('./database');
+const { body, validationResult } = require('express-validator');
 
-// Get the button that opens the modal
-var btnIn = document.getElementById("myBtnIn");
-var btnUp = document.getElementById("myBtnUp");
+const app = express();
+app.use(express.urlencoded({ extended: false }));
 
-// Get the <span> element that closes the modal
-var spanIn = document.getElementsByClassName("closeIn")[0];
-var spanUp = document.getElementsByClassName("closeUp")[0];
+// SET OUR VIEWS AND VIEW ENGINE
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.static('Pages'));
+app.use(express.static('Photos'));
 
-// When the user clicks on the button, open the modal
-btnIn.onclick = function() {
-    modalIn.style.display = "block";
-}
-btnUp.onclick = function() {
-    modalUp.style.display = "block";
-}
+// APPLY COOKIE SESSION MIDDLEWARE
+app.use(cookieSession({
+    name: 'session',
+    keys: ['key1', 'key2'],
+    maxAge: 3600 * 1000 // 1hr
+}));
 
-// When the user clicks on <span> (x), close the modal
-spanIn.onclick = function() {
-    modalIn.style.display = "none";
-}
-spanUp.onclick = function() {
-    modalUp.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-        if (event.target == modalIn) {
-            modalIn.style.display = "none";
-        } else if (event.target == modalUp) {
-            this.modalUp.style.display = "none";
-        }
+// DECLARING CUSTOM MIDDLEWARE
+const ifNotLoggedin = (req, res, next) => {
+    if (!req.session.isLoggedIn) {
+        return res.render('index');
     }
-    ///////////////////////////////////////////////////////////////////////////////////////
+    next();
+}
+
+const ifLoggedin = (req, res, next) => {
+        if (req.session.isLoggedIn) {
+            return res.redirect('/home');
+        }
+        next();
+    }
+    // END OF CUSTOM MIDDLEWARE
+
+// ROOT PAGE
+app.get('/', ifNotLoggedin, (req, res, next) => {
+    dbConnection.execute("SELECT `name` FROM `users` WHERE `id`=?", [req.session.userID])
+        .then(([rows]) => {
+            res.render('home', {
+                name: rows[0].name
+            });
+        });
+
+}); // END OF ROOT PAGE
+
+
+// REGISTER PAGE
+app.post('/register', ifLoggedin,
+    // post data validation(using express-validator)
+    [
+        body('email', 'Invalid email address!').isEmail().custom((value) => {
+            return dbConnection.execute('SELECT `email` FROM `users` WHERE `email`=?', [value])
+                .then(([rows]) => {
+                    if (rows.length > 0) {
+                        return Promise.reject('This E-mail already in use!');
+                    }
+                    return true;
+                });
+        }),
+        body('username', 'Username is Empty!').trim().not().isEmpty(),
+        body('pwd', 'The password must be of minimum length 6 characters').trim().isLength({ min: 6 }),
+    ], // end of post data validation
+    (req, res, next) => {
+
+        const validation_result = validationResult(req);
+        const { username, pwd, email } = req.body;
+        // IF validation_result HAS NO ERROR
+        if (validation_result.isEmpty()) {
+            // password encryption (using bcrypt)
+            bcrypt.hash(pwd, 12).then((hash_pass) => {
+                    // INSERTING USER INTO DATABASE
+                    dbConnection.execute("INSERT INTO `users`(`name`,`email`,`password`) VALUES(?,?,?)", [username, email, hash_pass])
+                        .then(result => {
+                            res.send(`your account has been created successfully, Now you can <a href="/">Login</a>`);
+                        }).catch(err => {
+                            // THROW INSERTING USER ERROR'S
+                            if (err) throw err;
+                        });
+                })
+                .catch(err => {
+                    // THROW HASING ERROR'S
+                    if (err) throw err;
+                })
+        } else {
+            // COLLECT ALL THE VALIDATION ERRORS
+            let allErrors = validation_result.errors.map((error) => {
+                return error.msg;
+            });
+            // REDERING index PAGE WITH VALIDATION ERRORS
+            res.render('index', {
+                register_error: allErrors,
+                old_data: req.body
+            });
+        }
+    }); // END OF REGISTER PAGE
+
+// LOGIN PAGE
+app.post('/', ifLoggedin, [
+    body('email').custom((value) => {
+        return dbConnection.execute('SELECT `email` FROM `users` WHERE `email`=?', [value])
+            .then(([rows]) => {
+                if (rows.length == 1) {
+                    return true;
+
+                }
+                return Promise.reject('Invalid Email Address!');
+
+            });
+    }),
+    body('pwd', 'Password is empty!').trim().not().isEmpty(),
+], (req, res) => {
+    const validation_result = validationResult(req);
+    const { pwd, email } = req.body;
+    if (validation_result.isEmpty()) {
+
+        dbConnection.execute("SELECT * FROM `users` WHERE `email`=?", [email])
+            .then(([rows]) => {
+                bcrypt.compare(pwd, rows[0].password).then(compare_result => {
+                        if (compare_result === true) {
+                            req.session.isLoggedIn = true;
+                            req.session.userID = rows[0].id;
+
+                            res.redirect('/');
+                        } else {
+                            res.render('index', {
+                                login_errors: ['Invalid Password!']
+                            });
+                        }
+                    })
+                    .catch(err => {
+                        if (err) throw err;
+                    });
+
+
+            }).catch(err => {
+                if (err) throw err;
+            });
+    } else {
+        let allErrors = validation_result.errors.map((error) => {
+            return error.msg;
+        });
+        // REDERING index PAGE WITH LOGIN VALIDATION ERRORS
+        res.render('index', {
+            login_errors: allErrors
+        });
+    }
+});
+// END OF LOGIN PAGE
+
+// LOGOUT
+app.get('/logout', (req, res) => {
+    //session destroy
+    req.session = null;
+    res.redirect('/');
+});
+// END OF LOGOUT
+
+app.use('/', (req, res) => {
+    res.status(404).send('<h1>404 Page Not Found!</h1>');
+});
+
+app.listen(3000, () => console.log("Server is Running..."));
